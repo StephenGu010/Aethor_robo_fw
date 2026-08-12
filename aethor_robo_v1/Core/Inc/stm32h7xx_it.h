995174a8a3023c3c816c869b76eb183bbe9319f9
wowguhvvh <1914435628@qq.com>
2026-08-12 17:48:57 +0800

    build: synchronize CubeMX USB dependencies

--- aethor_robo_v1/Core/Inc/stm32h7xx_it.h
+++ aethor_robo_v1/Core/Inc/stm32h7xx_it.h
@@ -56,9 +56,9 @@ void FDCAN1_IT0_IRQHandler(void);
 void FDCAN2_IT0_IRQHandler(void);
 void FDCAN1_IT1_IRQHandler(void);
 void FDCAN2_IT1_IRQHandler(void);
+void OTG_HS_IRQHandler(void);
 void FDCAN3_IT0_IRQHandler(void);
 void FDCAN3_IT1_IRQHandler(void);
-void OTG_HS_IRQHandler(void);
 void TIM23_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
--- aethor_robo_v1/Core/Src/stm32h7xx_it.c
+++ aethor_robo_v1/Core/Src/stm32h7xx_it.c
@@ -220,6 +220,20 @@ void FDCAN2_IT1_IRQHandler(void)
 }
 
 /**
+  * @brief This function handles USB On The Go HS global interrupt.
+  */
+void OTG_HS_IRQHandler(void)
+{
+  /* USER CODE BEGIN OTG_HS_IRQn 0 */
+
+  /* USER CODE END OTG_HS_IRQn 0 */
+  HAL_PCD_IRQHandler(&hpcd_USB_OTG_HS);
+  /* USER CODE BEGIN OTG_HS_IRQn 1 */
+
+  /* USER CODE END OTG_HS_IRQn 1 */
+}
+
+/**
   * @brief This function handles FDCAN3 interrupt 0.
   */
 void FDCAN3_IT0_IRQHandler(void)
@@ -245,20 +259,6 @@ void FDCAN3_IT1_IRQHandler(void)
   /* USER CODE BEGIN FDCAN3_IT1_IRQn 1 */
 
   /* USER CODE END FDCAN3_IT1_IRQn 1 */
-}
-
-/**
-  * @brief This function handles USB On The Go HS global interrupt.
-  */
-void OTG_HS_IRQHandler(void)
-{
-  /* USER CODE BEGIN OTG_HS_IRQn 0 */
-
-  /* USER CODE END OTG_HS_IRQn 0 */
-  HAL_PCD_IRQHandler(&hpcd_USB_OTG_HS);
-  /* USER CODE BEGIN OTG_HS_IRQn 1 */
-
-  /* USER CODE END OTG_HS_IRQn 1 */
 }
 
 /**
